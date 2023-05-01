f9ff29db4e2ed09daa36a0d3d74be033579af0c4
Yanis Kouidri <yanis.kouidri@gmail.com>
2023-05-01 18:12:30 +0200

    navbar refound

--- htdocs/Projets/E-portfolio/include/nav.php
+++ htdocs/Projets/E-portfolio/include/nav.php
@@ -1,40 +1,34 @@
-        <nav>
-            <ul class="navbar">
-                <li class="tab"> <a href="index.php"> Welcome </a>
-                    <ul>
-                        <li> <a href="index.php#about_me"> About me </a> </li>
-                        <li> <a href="index.php#video_cv">  Video CV </a> </li>
-                    </ul>
-                </li> 
+<nav>
+    <div class="navbar">
+        <a href="index.php">Welcome</a>
 
-                <li class="tab"> <a href="engineering_project.php"> Engineering projects </a>
-                    <ul>
-                        <li> <a href="engineering_project.php"> Comming soon </a> </li>
-                    </ul>
-                </li>
+        <a href="engineering_project.php">Engineering projects</a>
 
-                <li class="tab"> <a href="sustainable_development.php"> Sustainable Development </a>
-                </li>
+        <a href="sustainable_development.php">Sustainable Development</a>
 
-                <li class="tab"> <a href="International_mobility.php"> International mobility </a>
-                    <ul>
-                        <li> <a href="International_mobility.php"> Comming soon </a> </li>
-                    </ul>
-                </li>
+        <a href="International_mobility.php">International mobility</a>
 
-                <li class="tab"> <a href="professional_engagement.php"> Professional engagement </a>
-                    <ul>
-                        <li> <a href="./medias/Others/CV_Yanis_Kouidri_English.pdf"> CV </a> </li> 
-                        <li> <a href="https://www.linkedin.com/in/yanis-kouidri/"> Linkedin </a> </li> 
-                        <li> <a href="https://www.kouidri.fr"> Personal website </a> </li> 
-                    </ul>
-                </li>
+        <div class="dropdown">
+            <button class="dropbtn"> Professional engagement
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="professional_engagement.php"> My engagements </a>
+                <a href="./medias/Others/CV_Yanis_Kouidri_English.pdf"> CV </a>
+                <a href="https://www.linkedin.com/in/yanis-kouidri/"> Linkedin </a>
+                <a href="https://www.kouidri.fr"> Personal website </a>
+            </div>
+        </div>
 
-                <li class="tab"> More 
-                    <ul>
-                        <li> <a href="sport_and_co.php"> Sport & other activities  </a> </li>
-                        <li> <a href="about.php"> About this website  </a> </li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
+        <div class="dropdown">
+            <button class="dropbtn"> More
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="sport_and_co.php"> Sport & other activities  </a>
+                <a href="about.php"> About this website  </a>
+            </div>
+        </div>
+
+    </div> 
+</nav>
