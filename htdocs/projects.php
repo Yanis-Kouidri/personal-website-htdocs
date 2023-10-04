3ed3cf9bc9e84f0464dbb721822a94a868e7f731
Yanis Kouidri <yanis.kouidri@gmail.com>
2023-10-04 08:58:20 +0200

    Adding projects description

--- htdocs/projects.php
+++ htdocs/projects.php
@@ -10,47 +10,52 @@
     <?php include("includes/header.html") ?>
 
     <section id="accueil">
-        <h1>Bienvenue sur ma page web</h1>
-        <p>C'est la page des projets.</p>
+        <h1>Projets</h1>
+        <p>Cette page regroupe mes projets réalisés.</p>
     </section>
 
-    <?php
-        $repertoire = 'projects/'; // Remplacez ceci par le chemin vers votre dossier de projets
-
-        // Vérifiez si le répertoire existe
-        if (is_dir($repertoire)) {
-            // Ouvrez le répertoire
-            if ($dh = opendir($repertoire)) {
-                echo '<ul>';
-                
-                // Parcourez les fichiers et dossiers dans le répertoire
-                while (($fichier = readdir($dh)) !== false) {
-                    if ($fichier != '.' && $fichier != '..') {
-                        $chemin = $repertoire . $fichier;
-                        
-                        // Vérifiez si c'est un dossier
-                        if (is_dir($chemin)) {
-                            // Vérifiez s'il y a un fichier index.php ou index.html
-                            if (file_exists($chemin . '/index.php')) {
-                                echo '<li><a href="' . $chemin . '/index.php">' . $fichier . '</a></li>';
-                            } elseif (file_exists($chemin . '/index.html')) {
-                                echo '<li><a href="' . $chemin . '/index.html">' . $fichier . '</a></li>';
-                            } else {
-                                echo '<li><a href="' . $chemin . '">' . $fichier . '</a></li>';
-                            }
-                        }
-                    }
-                }
-                
-                echo '</ul>';
-                
-                // Fermez le répertoire
-                closedir($dh);
-            }
-        } else {
-            echo 'Le répertoire n\'existe pas.';
-        }
-        ?>
+    
+    <div>
+        <ul id=project-list>
+            <li class="projet"><div> 
+                <a target="_blank" href="projects/Trombinouc/main.php">
+                    <span>Trombinouc</span>  
+                </a> : Petit Facebook réalisé avec Maxime JUST lors sur module M2105 du DUT R&amp;T. Le but était de mettre en pratique nos compétences en PHP. </div>
+            </li>
+            <li class="projet"><div> 
+                <a target="_blank" href="projects/M2105/index.php">
+                    <span>Exercices PHP</span>  
+                </a> : Série de petits exercices en PHP afin de maitriser les bases de ce langage. Dans le cadre du module M2105 du DUT R&amp;T. </div>
+            </li>
+            <li class="projet"><div> 
+                <a target="_blank" href="projects/M1106-ProjetWeb/Page_principale/page_principale.html">
+                    <span>Quoi acheter</span>  
+                </a> : Site web de partage d'avis sur tout type de produits. Dans le cadre du module M1106 du DUT R&amp;T. Le but était d'écrire un site web uniquement en HTML 5 et CSS 3</div>
+            </li>
+            <li class="projet"><div> 
+                <a target="_blank" href="projects/M1106-JS/">
+                    <span>Introduction à JavaScript</span>  
+                </a> : Série de petits exercices en JavaScript en utilisant la bibliothèque de <a target="_blank" href="https://jquery.com/">Jquery</a> . Réalisé dans le cadre du module M1106 du DUT R&amp;T.</div>
+            </li>
+            <li class="projet"><div> 
+                <a target="_blank" href="https://github.com/Yanis-Kouidri/Projet-Python">
+                    <span>Découpeur de logs Apache</span>  
+                </a> : Programme en Python dont le but est de découper des logs Apache brutes, d'en faire un fichier json et à partir de ce json, de faire des statistiques. Réalisé dans le cadre du module M3206 du DUT R&amp;T.</div>
+            </li>
+            <li class="projet"><div> 
+                <a target="_blank" href="https://github.com/Quarfein/network-telecom-shell">
+                    <span>Implémentation d'un shell</span>  
+                </a> : Programme en C dont le but est d'interpréter des commandes taper par l'utilisateur. 
+                Il n'est pas aussi avancé que certains shells comme le sh et bash utilisés sur les systèmes UNIX mais c'est un très bon exercice pour apprendre à développer et comprendre comment fonctionne un shell. 
+                Réalisé dans le cadre d'un projet scolaire lors du DUT R&amp;T. Le projet a duré 3 semestres.</div>
+            </li>
+            <li class="projet"><div> 
+                <a target="_blank" href="projects/E-portfolio/index.php">
+                    <span>E-portfolio</span></a> 
+                : Site web de présentation réalisé dans le cadre du module Careers And Management (CAM) à l'ENSEEIHT.</div>
+            </li>
+        </ul>
+    </div>
 
 
 
