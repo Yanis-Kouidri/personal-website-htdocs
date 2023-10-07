1aec10d2b182432bdcb8b43f820ad987ec2155cf
Yanis Kouidri <yanis.kouidri@gmail.com>
2023-10-07 15:23:04 +0200

    updating bio

--- htdocs/index.php
+++ htdocs/index.php
@@ -14,9 +14,14 @@
 
     <section id="presentation">
         <h2>Présentation</h2>
-        <p>Je suis Yanis Kouidri, actuellement en deuxième année à l'ENSEEIHT, une grande école d'ingénieur basée à Toulouse
-             où je me spécialise dans les Sciences du Numérique. Mon parcours académique est axé sur l'alternance, 
-             ce qui signifie que je partage mon temps entre les cours à l'école et une expérience professionnelle en entreprise.</p>
+        <p>
+            Je suis <strong>Yanis Kouidri</strong>, actuellement en deuxième année à l’<strong>ENSEEIHT</strong>, une grande <strong>école d’ingénieur</strong> à Toulouse, je me spécialise dans la filière <em>Sciences du Numérique</em>. 
+            Mon parcours est enrichi par une expérience en <strong>alternance</strong>, me permettant d’alterner entre des périodes de cours et des périodes en entreprise. 
+            Cette approche me donne l’opportunité d’acquérir une solide expérience pratique et de me confronter directement au monde de l’entreprise tout en poursuivant mes études.
+            <br>
+            Je réalise mon alternance chez <strong>Thales Alenia Space</strong> à Toulouse, une branche de <em>Thales</em> spécialisée dans l’aérospatial. 
+            Dans le cadre de mon travail, j’étudie différents <strong>orchestrateurs de workflow</strong> afin d’élargir le panel d’outils utilisés par mon service pour le traitement des données des satellites d’observation de la Terre.
+        </p>
     </section>
 
     <?php include("includes/footer.html") ?>
