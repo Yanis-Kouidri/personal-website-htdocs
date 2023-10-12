6c0738ec6ae040a4dbab4195f436caba1974021b
Yanis Kouidri <yanis.kouidri@gmail.com>
2023-10-12 14:39:17 +0200

    updating trombinouc database cx

--- htdocs/projects/Trombinouc/Outils/base.php
+++ htdocs/projects/Trombinouc/Outils/base.php
@@ -1,10 +1,19 @@
 <?php
+    $db_servername = "db"; // Nom du service MySQL dans votre Docker Compose
+    $db_username = getenv('DB_USERNAME'); // Utilisateur MySQL
+    $db_password = getenv('DB_PASSWORD'); // Mot de passe défini lors de la création du conteneur MySQL
+    $db_name = "trombinouc"; // Remplacez par le nom de votre base de données
+
 
     try {
-        $bd=new PDO ("mysql:host=localhost;dbname=jm002743","jm002743","jm00274371ae");
-        $bd->exec('SET NAMES utf8');
+        $conn = new PDO("mysql:host=$db_servername;dbname=$db_name", $db_username, $db_password);
+
+        // Configuration pour afficher les erreurs PDO
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "Connexion à la base de données réussie";
+    } catch (PDOException $e) {
+        echo "Erreur de connexion : " . $e->getMessage();
     }
-    catch (Exception $e) {
-        die("Connexion impossible");
-    }
+
 ?>
