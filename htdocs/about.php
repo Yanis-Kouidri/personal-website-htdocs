791362940616c3adc18c2c11aa5b2368ff2a2090
Yanis Kouidri <yanis.kouidri@gmail.com>
2024-02-17 19:55:53 +0100

    Add links section to about.php and styles for links

--- htdocs/about.php
+++ htdocs/about.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <?php include("includes/head.html") ?>
     <title>Yanis Kouidri - A propos</title>
 </head>
@@ -12,9 +13,24 @@
         <p>Cette page est dédiée à ma présentation.</p>
     </section>
 
-    <section>
-        <h1>En construction</h1>
-    </section>
+    <section id="liens">
+    <h1>Mes Liens</h1>
+    <a href="https://github.com/Yanis-Kouidri" target="_blank">
+        <i class="fab fa-github"></i> GitHub
+    </a>
+    <a href="https://www.linkedin.com/in/yanis-kouidri" target="_blank">
+        <i class="fab fa-linkedin"></i> LinkedIn
+    </a>
+    <a href="./resources/CV/CV_Yanis_Kouidri_FR.pdf" target="_blank">
+        <i class="fas fa-file-alt"></i> CV - Français
+    </a>
+    <a href="./resources/CV/CV_Yanis_Kouidri_EN.pdf" target="_blank">
+        <i class="fas fa-file-alt"></i> CV - English
+    </a>
+
+
+</section>
+
 
 
     <?php include("includes/footer.html") ?>
